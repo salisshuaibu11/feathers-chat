404c5c7dc4ed89f657b53c3eb6eeee4972cbe03d
salis <salisshuaibu11@gmail.com>
2021-09-09 12:32:23 +0000

    Updated index.html and authentication.js file and als created a new client js file to establish authentication strategy

--- src/authentication.js
+++ src/authentication.js
@@ -1,13 +1,33 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require("@feathersjs/authentication");
+const { LocalStrategy } = require("@feathersjs/authentication-local");
+const { expressOauth } = require("@feathersjs/authentication-oauth");
 
-module.exports = app => {
+class GithubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      // You can also set the display name to profile.name
+      name: profile.login,
+      // The Github profile image
+      avatar: profile.avatar_url,
+      // The user email address (if available)
+      email: profile.email,
+    };
+  }
+}
+
+module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
+  authentication.register("github", new GithubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
 };
