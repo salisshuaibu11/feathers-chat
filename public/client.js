16a8465d6a06c193a47a33f59c8fffe8c804c98e
salis <salisshuaibu11@gmail.com>
2021-09-09 20:32:47 +0000

    Added some functions in clientjs

--- public/client.js
+++ public/client.js
@@ -29,6 +29,169 @@ const login = async () => {
   }
 };
 
+const loginHTML = `<main class="login container">
+  <div class="row">
+    <div class="col-12 col-6-tablet push-3-tablet text-center heading">
+      <h1 class="font-100">Log in or signup</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 col-6-tablet push-3-tablet col-4-desktop push-4-desktop">
+      <form class="form">
+        <fieldset>
+          <input class="block" type="email" name="email" placeholder="email">
+        </fieldset>
+
+        <fieldset>
+          <input class="block" type="password" name="password" placeholder="password">
+        </fieldset>
+
+        <button type="button" id="login" class="button button-primary block signup">
+          Log in
+        </button>
+
+        <button type="button" id="signup" class="button button-primary block signup">
+          Sign up and log in
+        </button>
+
+        <a class="button button-primary block" href="/oauth/github">
+          Login with GitHub
+        </a>
+      </form>
+    </div>
+  </div>
+</main>`;
+
+// Chat base HTML (without user list and messages)
+
+const chatHTML = `<main class="flex flex-column">
+  <header class="title-bar flex flex-row flex-center">
+    <div class="title-wrapper block center-element">
+      <img class="logo" src="http://feathersjs.com/img/feathers-logo-wide.png"
+        alt="Feathers Logo">
+      <span class="title">Chat</span>
+    </div>
+  </header>
+
+  <div class="flex flex-row flex-1 clear">
+    <aside class="sidebar col col-3 flex flex-column flex-space-between">
+      <header class="flex flex-row flex-center">
+        <h4 class="font-300 text-center">
+          <span class="font-600 online-count">0</span> users
+        </h4>
+      </header>
+
+      <ul class="flex flex-column flex-1 list-unstyled user-list"></ul>
+      <footer class="flex flex-row flex-center">
+        <a href="#" id="logout" class="button button-primary">
+          Sign Out
+        </a>
+      </footer>
+    </aside>
+
+    <div class="flex flex-column col col-9">
+      <main class="chat flex flex-column flex-1 clear"></main>
+
+      <form class="flex flex-row flex-space-between" id="send-message">
+        <input type="text" name="text" class="flex flex-1">
+        <button class="button-primary" type="submit">Send</button>
+      </form>
+    </div>
+  </div>
+</main>`;
+
+// Helper to safely escape HTML
+const escape = (str) =>
+  str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+// Add a new user to the list
+const addUser = (user) => {
+  const userList = document.querySelector(".user-list");
+
+  if (userList) {
+    // Add the user to the list
+    userList.innerHTML += `<li>
+      <a class="block relative" href="#">
+        <img src="${user.avatar}" alt="" class="avatar">
+        <span class="absolute username">${escape(
+          user.name || user.email
+        )}</span>
+      </a>
+    </li>`;
+
+    // Update the number of users
+    const userCount = document.querySelector(".user-list li").length;
+
+    document.querySelector(".online-count").innerHTML = userCount;
+  }
+};
+
+// Renders a message to the page
+const addMessage = (message) => {
+  // The user that sent this message (added by the populate-user hook)
+  const { user = {} } = message;
+  const chat = document.querySelector(".chat");
+
+  // Escape HTML to prevent XSS attacks
+  const text = escape(message.text);
+
+  if (chat) {
+    chat.innerHTML += `<div class="message flex flex-row">
+      <img src="${user.avatar}" alt="${user.name || user.email}" class="avatar">
+      <div class="message-wrapper">
+        <p class="message-header">
+          <span class="username font-600">${escape(
+            user.name || user.email
+          )}</span>
+          <span class="sent-date font-300">${moment(message.createdAt).format(
+            "MMM Do, hh:mm:ss"
+          )}</span>
+        </p>
+        <p class="message-content font-300">${text}</p>
+      </div>
+    </div>`;
+
+    // Always scroll to the bottom of our message list
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+  }
+};
+
+// Show the login page
+const showLogin = (error) => {
+  if (document.querySelectorAll(".login").length && error) {
+    document
+      .querySelector(".heading")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<p>There was an error: ${error.message}</p>`
+      );
+  } else {
+    document.getElementById("app").innerHTML = loginHTML;
+  }
+};
+
+// Shows the chat page
+const showChat = async () => {
+  document.getElementById("app").innerHTML = chatHTML;
+
+  // Find the latest 25 messages. They will come with the newest first
+  const messages = await client.service("messages").find({
+    query: {
+      $sort: { createdAt: -1 },
+      $limit: 25,
+    },
+  });
+
+  // We want to show the newest message last
+  messages.data.reverse().forEach(addMessage);
+
+  // Find all users
+  const users = await client.service("users").find();
+
+  // Add each user to the list
+  users.data.forEach(addUser);
+};
+
 const main = async () => {
   const auth = await login();
 
